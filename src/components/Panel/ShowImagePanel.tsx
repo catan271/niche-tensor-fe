9a25592cb45e401caaa3afa1ca349fec9d5507b2
QuangTran <quangtrancnh@gmail.com>
2024-02-07 15:10:26 +0700

    add showimagepanel

--- src/components/Panel/ShowImagePanel.tsx
+++ src/components/Panel/ShowImagePanel.tsx
@@ -1,4 +1,33 @@
+import React, { useEffect } from 'react';
+import SvgIcon from '@/svg/SvgIcon';
+
 const ShowImagePanel = () => {
-    return <div className="bg-background dark:bg-background">ShowImagePanel</div>;
+    const [isLoading, setIsLoading] = React.useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
+    }, []);
+
+    return (
+        <div className="w-full h-full mx-auto">
+            {isLoading ? (
+                <div className="flex animate-pulse flex-row pt-20 h-full justify-center space-x-5">
+                    <div className="w-500 bg-placeholder h-500 rounded-md "></div>
+                </div>
+            ) : (
+                <div className="flex flex-col p-20 rounded-md justify-center bg-input space-x-5">
+                    <img
+                        className="h-auto max-w-full object-contain rounded-sm"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                        alt=""
+                    />
+                    <div className="flex flex-row justify-center pt-20">
+                        <SvgIcon type={'Download'} />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 export default ShowImagePanel;
--- src/svg/SvgIcon.tsx
+++ src/svg/SvgIcon.tsx
@@ -5,6 +5,38 @@ interface SvgIconInterface {
 }
 const SvgIcon: React.FC<SvgIconInterface> = ({ type }) => {
     switch (type) {
+        case 'Download':
+            return (
+                <svg
+                    width="40px"
+                    height="40px"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    stroke="#00a1d7"
+                    className="cursor-pointer"
+                >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        {' '}
+                        <path
+                            d="M12 7L12 14M12 14L15 11M12 14L9 11"
+                            stroke="#00a1d7"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>{' '}
+                        <path d="M16 17H12H8" stroke="#00a1d7" strokeWidth="1.5" strokeLinecap="round"></path>{' '}
+                        <path
+                            opacity="0.5"
+                            d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
+                            stroke="#00a1d7"
+                            strokeWidth="1.5"
+                        ></path>{' '}
+                    </g>
+                </svg>
+            );
         case 'Rectangle':
             return (
                 <svg
