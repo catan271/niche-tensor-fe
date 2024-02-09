2e60eeef5af363c789e33488df43a9ae2745bb9a
Haha02012021 <daothuhang2192002@gmail.com>
2024-02-09 22:46:50 +0700

    add example panel & download img

---
+++ src/client/constants/exampleData.ts
@@ -0,0 +1,18 @@
+export const exampleData = [
+    {
+        image: 'example1.png',
+        prompt: 'High resolution photo of a miniature Legend of Zelda village.',
+    },
+    {
+        image: 'example2.png',
+        prompt: '35mm matte photo of a shallow straw basket full of apples on a wooden table in a magical forest.',
+    },
+    {
+        image: 'example3.png',
+        prompt: 'Full view portrait mystical ethereal herbs goddess wearing a beautiful dress, herbs dryad.',
+    },
+    {
+        image: 'example4.png',
+        prompt: '8k resolution beautiful cozy riverside solarpunk inviting Hobbit-House treehouse digital illustration matte painting.',
+    },
+];
