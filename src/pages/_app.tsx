bde477246dfff6ed3cce7072c41e66a86878589d
catan271 <canh.tranduc2002@gmail.com>
2024-02-09 15:22:55 +0700

    update: page title & icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import type { AppProps } from 'next/app';
 import '@/client/styles/theme.less';
 import '@/client/styles/globals.scss';
 import MainLayout from '@/client/components/Layout/MainLayout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <MainLayout>
-            <Component {...pageProps} />
-        </MainLayout>
+        <>
+            <MainLayout>
+                <Head>
+                    <title>NicheTensor - AI image generator</title>
+                </Head>
+                <Component {...pageProps} />
+            </MainLayout>
+        </>
     );
 }
